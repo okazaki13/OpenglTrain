4cc13f8c872da6438d3b68e3ee0e185052ac39c0
okazaki13 <atelier.ok.ss@gmail.com>
2019-02-08 07:22:28 +0900

    voxelデータ追加

--- OpenGLTrain/OpenGLTrain/Voxel.cpp
+++ OpenGLTrain/OpenGLTrain/Voxel.cpp
@@ -5,6 +5,11 @@
 
 Voxel::Voxel()
 {
+	color[0] = 124;
+	color[1] = 177;
+	color[2] = 255;
+	color[3] = 50;
+	size = 1;
 }
 
 
@@ -13,20 +18,21 @@ Voxel::~Voxel()
 }
 
 
-void Voxel::create_voxel(int offset[2]) {
+void Voxel::create_voxel(int offset[3]) {
 	//立方体
 	glPushMatrix();
-	glColor3d(color[0], color[1], color[2]);//色の設定
+	glClearColor(color[0], color[1], color[2],color[3]);//色の設定.引数は４つでRGBA（Aは透明度）
 	glTranslated(offset[0], offset[1],offset[2]);//平行移動値の設定
 	glutSolidCube(size);//引数：(一辺の長さ)
 	glPopMatrix();
 
 }
 
+//cube一つだけ生成するとき
 void Voxel::create_voxel(int offset_x, int offset_y, int offset_z) {
 	//立方体
 	glPushMatrix();
-	glColor3d(color[0], color[1], color[2]);//色の設定
+	glClearColor(color[0], color[1], color[2],color[3]);//色の設定。引数は４つでRGBA（Aは透明度）
 	glTranslated(offset_x, offset_y,offset_z);//平行移動値の設定
 	glutSolidCube(size);//引数：(一辺の長さ)
 	glPopMatrix();
@@ -41,4 +47,20 @@ void Voxel::set_voxelcolor(double R, double G, double B) {
 
 void Voxel::set_voxelsize(double s) {
 	size = s;
+}
+
+//ボクセルで立方体か長方形のオブジェクトを作るとき
+void Voxel::CreateVoxelObject(int a, int b, int c) {
+	for (int i; i < a; i++) {
+		for (int j; j < b; j++) {
+			for (int k; k < c; k++) {
+				glPushMatrix();
+				glClearColor(color[0], color[1], color[2], color[3]);//色の設定
+				glTranslated(i, j, k);//平行移動値の設定
+				glutSolidCube(size);//引数：(一辺の長さ)
+				voxelID[i][j][k] = true;
+				glPopMatrix();
+			}
+		}
+	}
 }
--- OpenGLTrain/OpenGLTrain/Voxel.h
+++ OpenGLTrain/OpenGLTrain/Voxel.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <vector>
+//using namespace std ヘッダファイルでは御法度らしい
+
 class Voxel
 {
 	/*ボクセルを作るためのクラス
@@ -11,13 +14,13 @@ public:
 	void create_voxel(int offset_x,int offset_y,int offset_z);
 	void set_voxelcolor(double R,double G,double B);
 	void set_voxelsize(double size);
+	void CreateVoxelObject(int a, int b, int c);
 
 private:
-	int centor[2];
-	int position[2];
-	double color[2];
-	double size;
-
+	int centor[3];//ボクセル中心点
+	float color[4];//ボクセルの色.引数は４つでRGBA（Aは透明度）
+	double size;//ボクセルの大きさ
+	std::vector<std::vector<std::vector<bool>>> voxelID;//int型３次元配列でボクセルそれぞれの有無を管理
 
 
 
