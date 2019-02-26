42b9aff50bddb8348be748625fb43fe6cbaf8099
atelier-okazaki <masa1ken2tetu3@yahoo.co.jp>
2019-02-26 12:52:10 +0900

    STLmodel修正

---
+++ OpenGLTrain/OpenGLTrain/global_bariables.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../../include/SpidarAHS/SpidarAHS.h"
+
+
+
+
+//スパイダーのハンドルと受け取る情報を格納する変数
+
+extern float spidar_origin_quaternion[4];//クォータニオン
+extern float spidar_origin_position[3];//位置
+extern float spidar_origin_velocity[3];//速度
